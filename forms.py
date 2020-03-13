ae7154f08574c2c1850ca436fbdb497fb1bf8da5
Ramil <ramil9898@gmail.com>
2020-03-12 23:35:15 -0400

    Created SQLAlchemy db file and User and Post Models

--- forms.py
+++ forms.py
@@ -2,17 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', 
-		validators=[DataRequired(), Length(min=5, max=20)])
+		validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email',
 		validators=[DataRequired(), Email()])
 	password = PasswordField('Password',
 		validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password',
 		validators=[DataRequired(), EqualTo('password')])
-
 	submit = SubmitField('Sign Up')
 
 
@@ -22,5 +20,4 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password',
 		validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
-
 	submit = SubmitField('Login')
