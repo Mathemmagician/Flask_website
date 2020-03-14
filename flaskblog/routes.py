b46173d9140da96ce3d56709578faa8f8f607d25
Ramil <ramil9898@gmail.com>
2020-03-13 21:27:35 -0400

    Added an automatic square cropping for images

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -72,6 +72,16 @@ def logout():
     return redirect(url_for('home'))
 
 
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -79,7 +89,7 @@ def save_picture(form_picture):
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
     
     output_size = (250, 250)  # Resize image
-    i = Image.open(form_picture)
+    i = crop_max_square(Image.open(form_picture))
     i.thumbnail(output_size, Image.ANTIALIAS)
 
     i.save(picture_path)
