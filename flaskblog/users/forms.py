e2d131dc64790980aa3913b87a778d2689d72a52
Ramil <ramil9898@gmail.com>
2020-03-15 09:42:11 -0400

    Blueprints baby (exclamation mark)

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
 
 
@@ -58,12 +58,6 @@ class UpdateAccountForm(FlaskForm):
 				raise ValidationError('That email is taken')
 
 
-class PostForm(FlaskForm):
-	title = StringField('Title', validators=[DataRequired()])
-	content = TextAreaField('Content', validators=[DataRequired()])
-	submit = SubmitField('Post')
-	
-
 class RequestResetForm(FlaskForm):
 	email = StringField('Email',
 		validators=[DataRequired(), Email()])
@@ -81,10 +75,6 @@ class ResetPasswordForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password',
 		validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Reset Password')
-
-
-
-
 
 
 
---
+++ flaskblog/users/utils.py
@@ -0,0 +1,44 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for, current_app
+from flask_mail import Message
+from flaskblog import mail
+
+
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (250, 250)  # Resize image
+    i = crop_max_square(Image.open(form_picture))
+    i.thumbnail(output_size, Image.ANTIALIAS)
+
+    i.save(picture_path)
+    return picture_fn
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', 
+        sender='noreply@demo.com', 
+        recipients=[user.email])
+
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('users.reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no change will be made.
+'''
+    mail.send(msg)
