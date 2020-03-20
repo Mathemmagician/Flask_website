6fb3f52099f04939692a83aedefbb816fb324436
Ramil_server <ramil9898@gmail.com>
2020-03-20 16:27:03 +0000

    Server Side Git Sync

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -33,7 +33,6 @@ def save_picture(form_picture):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request', 
-        sender='noreply@demo.com', 
         recipients=[user.email])
 
     msg.body = f'''To reset your password, visit the following link:
