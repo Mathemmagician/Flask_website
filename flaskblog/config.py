802a444704f25f058647ff79c24541a8ed4dc32e
Ramil_server <ramil9898@gmail.com>
2020-03-20 17:35:03 +0000

    Minor

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -11,12 +11,6 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-<<<<<<< HEAD
-    MAIL_DEFAULT_SENDER = config.get('EMAIL_USER')
-    
+    #MAIL_DEFAULT_SENDER = config.get('EMAIL_USER')
     MAIL_USERNAME = config.get('EMAIL_USER')
     MAIL_PASSWORD = config.get('EMAIL_PASS')
-=======
-    MAIL_USERNAME = "ramilraleskerov@gmail.com" #os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = "TimeTo1mprove" #os.environ.get('EMAIL_PASS')
->>>>>>> e9fdd74e256f36e89d5a8078b63174eaa0f60cc4
